1b4b7948b31799c4ab669973e394adb5ac7135f7
adeloaleman <adeloaleman@gmail.com>
2026-03-04 16:01:10 +0000

    Commit

--- backend/db.ts
+++ backend/db.ts
@@ -1,53 +1,27 @@
 import { createConnection } from "typeorm";
 
-import { Users }    from "./src/backend/entities/users";
-import { Tweet }    from "./src/backend/entities/tweet";
-import { Comment }  from "./src/backend/entities/comment";
-import { Like }     from "./src/backend/entities/like";
-
+import { Users }   from "./src/backend/entities/users";
+import { Tweet }   from "./src/backend/entities/tweet";
+import { Comment } from "./src/backend/entities/comment";
+import { Like }    from "./src/backend/entities/like";
 
 export async function createDbConnection() {
-    // // Read the database settings from the environment variables
-    // export DATABASE_HOST=localhost
-    // export DATABASE_PORT=5432
-    // export DATABASE_DB=twitterclone_db
-    // export DATABASE_USER=postgres
-    // export DATABASE_PASSWORD=secret
-    
-    // export AUTH_SECRET=miclaveprivada
-    const DATABASE_HOST     = process.env.DATABASE_HOST;
-    const DATABASE_PORT     = process.env.DATABASE_PORT;
-    const DATABASE_DB       = process.env.DATABASE_DB;
-    const DATABASE_USER     = process.env.DATABASE_USER;
-    const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+  // SQLite database file (will be created if it doesn't exist)
+  const DATABASE_FILE = "twitterclone.sqlite";
 
-    // Display the settings in the console so we can see if something is wrong
-    console.log(
-        `
-            host:     ${DATABASE_HOST}
-            port:     ${DATABASE_PORT}
-            db:       ${DATABASE_DB}
-            user:     ${DATABASE_USER}
-            password: ${DATABASE_PASSWORD}
-        `
-    );
+  console.log(`
+    Using SQLite database file: ${DATABASE_FILE}
+  `);
 
-    // Open database connection
-    await createConnection({
-        type:     "postgres",
-        host:     DATABASE_HOST,
-        port:     5432,
-        username: DATABASE_USER,
-        password: DATABASE_PASSWORD,
-        database: DATABASE_DB,
-        entities: [
-            Comment,
-            Tweet,
-            Users,
-            Like
-        ],
-        // This setting will automatically create database tables in the database server
-        synchronize: true
-    });
-
+  await createConnection({
+    type: "sqlite",
+    database: DATABASE_FILE,
+    entities: [
+      Comment,
+      Tweet,
+      Users,
+      Like
+    ],
+    synchronize: true
+  });
 }
